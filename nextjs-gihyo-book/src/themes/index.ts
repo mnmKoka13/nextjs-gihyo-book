1c02819d191dc6cbf359aed0e8163714be4f74fc
Manami Kokado <kokado313.mnm@gmail.com>
2024-09-23 19:41:31 +0900

    Nextjs勉強 20240923

---
+++ nextjs-gihyo-book/src/themes/index.ts
@@ -0,0 +1,13 @@
+import fontSizes from "./fontSizes"
+import letterSpacings from "./letterSpacing"
+import space from "./space"
+import lineHeights from "./lineHeights"
+import colors from "./colors"
+
+export const theme = {
+  space,
+  fontSizes,
+  letterSpacings,
+  lineHeights,
+  colors,
+} as const
